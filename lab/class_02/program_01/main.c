26ab4c7ce3db79456f666dee3d61f823fcfbf20d
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-19 19:39:14 +0530

    class 02 program 01

---
+++ lab/class_02/program_01/main.c
@@ -0,0 +1,44 @@
+// Bubble Sort
+
+#include <stdio.h>
+
+void BubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void input(int arr[], int n) {
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void display(int arr[], int n) {
+    printf("The sorted array...\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    input(arr, n);
+    BubbleSort(arr, n);
+    display(arr, n);
+
+    return 0;
+}
