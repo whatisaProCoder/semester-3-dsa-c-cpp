cbce5a96455d4e407b2596df77a6aaa5dd58ae7c
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-20 10:07:48 +0530

    class_03

---
+++ class_03/program_01/main.c
@@ -0,0 +1,57 @@
+// Selection Sort
+
+#include <stdio.h>
+
+void SelectionSort(int arr[], int n) {
+    int i, j, k;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = k = i; j < n; j++) {
+            if (arr[j] < arr[k]) k = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+}
+
+void input(int arr[], int n) {
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void display(int arr[], int n) {
+    printf("The sorted array...\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    input(arr, n);
+    SelectionSort(arr, n);
+    display(arr, n);
+
+    return 0;
+}
+
+/*OUTPUT
+Enter number of elements: 5
+Enter the elements:
+1
+7
+3
+5
+4
+The sorted array...
+1 3 4 5 7
+*/
---
+++ class_03/program_02/main.c
@@ -0,0 +1,78 @@
+// Quicksort Sort
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int l, int h) {
+    int pivot = arr[l];
+    int i = l, j = h;
+
+    do {
+        do {
+            i++;
+        } while (arr[i] <= pivot);
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i < j) swap(&arr[i], &arr[j]);
+    } while (i < j);
+
+    swap(&arr[l], &arr[j]);
+    return j;
+}
+
+void QuickSort(int arr[], int l, int h) {
+    int j;
+    if (l < h) {
+        j = partition(arr, l, h);
+        QuickSort(arr, l, j);
+        QuickSort(arr, j + 1, h);
+    }
+}
+
+void input(int arr[], int n) {
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void display(int arr[], int n) {
+    printf("The sorted array...\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    input(arr, n);
+    QuickSort(arr, 0, n - 1);
+    display(arr, n);
+
+    return 0;
+}
+
+/*OUTPUT
+Enter number of elements: 5
+Enter the elements:
+1
+7
+3
+5
+4
+The sorted array...
+1 3 4 5 7
+*/
