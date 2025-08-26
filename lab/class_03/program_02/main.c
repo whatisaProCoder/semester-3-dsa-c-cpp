33520f98008c09ffbb620cd26f60e23cafa3a3f8
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-27 01:55:33 +0530

    fix

--- lab/class_03/program_02/main.c
+++ lab/class_03/program_02/main.c
@@ -3,66 +3,66 @@
 #include <stdio.h>
 
 void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 int partition(int arr[], int l, int h) {
-    int pivot = arr[l];
-    int i = l, j = h;
+  int pivot = arr[l];
+  int i = l, j = h;
 
+  do {
     do {
-        do {
-            i++;
-        } while (arr[i] <= pivot);
-        do {
-            j--;
-        } while (arr[j] > pivot);
+      i++;
+    } while (i < h && arr[i] <= pivot);
+    do {
+      j--;
+    } while (j > l && arr[j] > pivot);
 
-        if (i < j) swap(&arr[i], &arr[j]);
-    } while (i < j);
+    if (i < j) swap(&arr[i], &arr[j]);
+  } while (i < j);
 
-    swap(&arr[l], &arr[j]);
-    return j;
+  swap(&arr[l], &arr[j]);
+  return j;
 }
 
 void QuickSort(int arr[], int l, int h) {
-    int j;
-    if (l < h) {
-        j = partition(arr, l, h);
-        QuickSort(arr, l, j);
-        QuickSort(arr, j + 1, h);
-    }
+  int j;
+  if (l < h) {
+    j = partition(arr, l, h);
+    QuickSort(arr, l, j);
+    QuickSort(arr, j + 1, h);
+  }
 }
 
 void input(int arr[], int n) {
-    printf("Enter the elements: \n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+  printf("Enter the elements: \n");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
 }
 
 void display(int arr[], int n) {
-    printf("The sorted array...\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+  printf("The sorted array...\n");
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
 }
 
 int main() {
-    int n;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+  int n;
+  printf("Enter number of elements: ");
+  scanf("%d", &n);
 
-    int arr[n];
+  int arr[n];
 
-    input(arr, n);
-    QuickSort(arr, 0, n - 1);
-    display(arr, n);
+  input(arr, n);
+  QuickSort(arr, 0, n - 1);
+  display(arr, n);
 
-    return 0;
+  return 0;
 }
 
 /*OUTPUT
