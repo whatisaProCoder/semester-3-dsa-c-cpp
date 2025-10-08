026a7d11975be530d59cd4fcd1a1617112d0c134
Pritam Debnath <104326867+whatisaProCoder@users.noreply.github.com>
2025-10-08 05:16:14 +0000

    refactor

---
+++ exam/midsem/question_2/main.c
@@ -0,0 +1,5 @@
+/*
+Take the same array and apply quick sort. Try to find the element 8 in 
+the sorted array using optimal method. If not found insert the element 
+in its proper position in the sorted array.
+*/
